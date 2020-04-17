e0388413adfcaa257e21afbc538aeab68b476d12
Camilla "Nanie" Camatta <camillacamatta@gmail.com>
2020-04-17 15:44:03 -0300

    FirstUpdate

---
+++ Runtime/EventController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace com.vrglass.eventcontroller
+{
+    public abstract class EventController<T> : MonoBehaviour
+    {
+        public abstract void NotifyController(T notifyEvent, object[] parameters);
+    }
+}
+
---
+++ Samples/SampleControllers/Scripts/Controllers/SampleController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using com.vrglass.eventcontroller;
+
+namespace com.vrglass.eventcontroller
+{
+    //You need to specify the event type of the controller
+    public class SampleController : EventController<NOTIFY_EVENTS>
+    {
+        public override void NotifyController(NOTIFY_EVENTS notifyEvent, object[] parameters)
+        {
+            switch (notifyEvent)
+            {
+                case NOTIFY_EVENTS.EVENT_SAMPLE:
+                    print("Sample event fired message: " + parameters[0].ToString());
+                    break;
+            }
+        }
+    }
+}
